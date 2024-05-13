b89e1a6205d095adeebe32d491345b2e1c0cad60
Steven Rey <stevenreybritez@gmail.com>
2024-05-13 11:17:04 -0400

    gtag

--- app/layout.tsx
+++ app/layout.tsx
@@ -72,6 +72,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <meta name="theme-color" media="(prefers-color-scheme: light)" content="#fff" />
       <meta name="theme-color" media="(prefers-color-scheme: dark)" content="#000" />
       <link rel="alternate" type="application/rss+xml" href="/feed.xml" />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-NY2VV9F1DR"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-NY2VV9F1DR');
+      </script>
       <body className="bg-white pl-[calc(100vw-100%)] text-black antialiased dark:bg-gray-950 dark:text-white">
         <ThemeProviders>
           <Analytics analyticsConfig={siteMetadata.analytics as AnalyticsConfig} />
